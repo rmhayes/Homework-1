ef5332a640716fec2ce9b64f78fb810ae4e47b97
Ryan Hayes <r.hayes@berkeley.edu>
2017-09-14 18:19:54 -0700

    added own code

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,23 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		if (position == 0 || position == 1) {
+			return position;
+		} else {
+			uint fib1 = 1;
+			uint fib2 = 1;
+			uint index = 1;
+			uint temp;
+			while (index < position) {
+				temp = fib2;
+				fib2 = fib1 + fib2;
+				fib1 = temp;
+				index = index + 1;
+			}
+			return fib2;
+		}
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() {}
 }
